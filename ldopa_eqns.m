1bf15b8b4cf0731ec31671f8df8f0546a5d457fd
TSBed <39437824+TSBed@users.noreply.github.com>
2020-04-12 22:02:31 -0500

    Base equations for ode45
    These are the differential equations that ode45 will call upon for our model. Note the order of parameters in the file, this will be important to keep consistent with the driver file.

---
+++ ldopa_eqns.m
@@ -0,0 +1,26 @@
+function dydt = ldopa_eqns(t,y,p)
+% This function defines equations to simulate how l-dopa affects dopamine
+% concentration.
+
+%% Parameters
+% I will just be defining paramaters here
+ka = p(1); %Absorption from gut
+k_cl = p(2); %Clearence rate of levopa from central
+k_cl_dop = p(3); %Clearence rate of dopamine from central
+k_met = p(4); %Metabolism rate of l-dopa to dopamine
+k_prod = p(5); %Production rate of dopamine in the brain
+k_abs = p(6); %Absorption rate of dopamine in the brain
+k12 = p(7); %Transport from the central to brain
+k21 = p(8); %Transport from brain to central
+V1 = p(9); % Central compartment volume
+V2 = p(10); % Brain compartment volume
+
+%% Equations
+dydt = zeros(5,1);
+dydt(1) = -ka * y(1); %Drug in the gut
+dydt(2) = ka * y(1) - k_cl * y(2) - k12 * y(2) - k_met * y(2) + k21 * y(4) * V2 / V1; %l-dopa in the blood stream
+dydt(3) = k_met * y(2) - k_cl_dop * y(3); %dopamine in blood stream
+dydt(4) = k12 * y(2) * V1 / V2 - k21 * y(4) - k_met * y(4); %l-dopa in the brain
+dydt(5) = k_met*y(4) + k_prod - k_abs * y(5); %dopamine in the brain
+
+end
