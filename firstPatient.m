17d65ec1c4945c7c532e27d74411f1e365eab9c9
TSBed <39437824+TSBed@users.noreply.github.com>
2020-04-14 22:24:32 -0500

    Updated files to have new dif eqs
    Changed the absorption rate by volume to achieve molecular balance. Need to add the lung bit and modulate some of the parameters appropriately

--- firstPatient.m
+++ firstPatient.m
@@ -17,12 +17,13 @@ k_cl_dop = 20.49;
 
 %% Actual Simulation
 
-y0 = [D0 0 0 0 .7]; %NOTE THERE NEEDS TO BE AN INITIAL DOPAMINE CONCENTRATION I NEED TO DO THE MATH ON THAT
+y0 = [D0 0 0 0 .72 0]; %NOTE THERE NEEDS TO BE AN INITIAL DOPAMINE CONCENTRATION I NEED TO DO THE MATH ON THAT
 p = [ka k_cl k_cl_dop k_met k_prod k_abs k12 k21 V1 V2];
 
 
 options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
 [T1,Y1] = ode45(@ldopa_eqns,[0 50],y0,options,p); % simulate model
+MassBal = ((Y1 * [1 V1 0 V2 0 1].') - D0)/D0;
 
 
 figure;
@@ -30,18 +31,22 @@ subplot(2,2,1);
 plot(T1,Y1(:,2));
 title("L-dopa Central")
 xlabel("Hours")
+ylabel("nM");
 
 subplot(2,2,2);
-plot(T1,Y1(:,3));
-title("Dopamine Central")
-xlabel("Hours")
-
-subplot(2,2,3);
 plot(T1,Y1(:,4));
 title("L-dopa Brain")
 xlabel("Hours")
+ylabel("nM");
 
-subplot(2,2,4);
+subplot(2,2,3);
 plot(T1,Y1(:,5));
 title("Dopamine Brain")
 xlabel("Hours")
+ylabel("nM");
+
+subplot(2,2,4);
+plot(T1,MassBal);
+title("Mass Balance");
+xlabel("Hours")
+ylabel("Deviation");
--- ldopa_eqns.m
+++ ldopa_eqns.m
@@ -3,7 +3,8 @@ function dydt = ldopa_eqns(t,y,p)
 % concentration.
 
 %% Parameters
-% I will just be defining paramaters here
+% I will just be defining paramaters here. Some of these do NOT change.
+
 ka = p(1); %Absorption from gut
 k_cl = p(2); %Clearence rate of levopa from central
 k_cl_dop = p(3); %Clearence rate of dopamine from central
@@ -15,12 +16,14 @@ k21 = p(8); %Transport from brain to central
 V1 = p(9); % Central compartment volume
 V2 = p(10); % Brain compartment volume
 
+k12 = k12 * V2/V1;
 %% Equations
-dydt = zeros(5,1);
-dydt(1) = -ka * y(1); %Drug in the gut
+dydt = zeros(6,1);
+dydt(1) = -ka * V1 *  y(1); %Drug in the gut
 dydt(2) = ka * y(1) - k_cl * y(2) - k12 * y(2) - k_met * y(2) + k21 * y(4) * V2 / V1; %l-dopa in the blood stream
 dydt(3) = k_met * y(2) - k_cl_dop * y(3); %dopamine in blood stream
 dydt(4) = k12 * y(2) * V1 / V2 - k21 * y(4) - k_met * y(4); %l-dopa in the brain
 dydt(5) = k_met*y(4) + k_prod - k_abs * y(5); %dopamine in the brain
+dydt(6) = k_cl*y(2)*V1 + k_met*y(2)*V1 + k_met*y(4)*V2; %Mass balance
 
 end
