d4a45c56b0ac8726577bd77d0851069d9e0bfeff
TSBed <39437824+TSBed@users.noreply.github.com>
2020-04-15 10:04:59 -0500

    Minor changes
    runPatient is the function version of firstPatient

--- ldopa_eqns.m
+++ ldopa_eqns.m
@@ -15,15 +15,17 @@ k12 = p(7); %Transport from the central to brain
 k21 = p(8); %Transport from brain to central
 V1 = p(9); % Central compartment volume
 V2 = p(10); % Brain compartment volume
+ka_lung = p(11); %Lung absorption rate
 
 k12 = k12 * V2/V1;
 %% Equations
-dydt = zeros(6,1);
+dydt = zeros(7,1);
 dydt(1) = -ka * V1 *  y(1); %Drug in the gut
-dydt(2) = ka * y(1) - k_cl * y(2) - k12 * y(2) - k_met * y(2) + k21 * y(4) * V2 / V1; %l-dopa in the blood stream
+dydt(2) = ka * y(1) + ka_lung * y(6) - k_cl * y(2) - k12 * y(2) - k_met * y(2) + k21 * y(4) * V2 / V1; %l-dopa in the blood stream
 dydt(3) = k_met * y(2) - k_cl_dop * y(3); %dopamine in blood stream
 dydt(4) = k12 * y(2) * V1 / V2 - k21 * y(4) - k_met * y(4); %l-dopa in the brain
 dydt(5) = k_met*y(4) + k_prod - k_abs * y(5); %dopamine in the brain
-dydt(6) = k_cl*y(2)*V1 + k_met*y(2)*V1 + k_met*y(4)*V2; %Mass balance
+dydt(6) = -ka_lung*y(6)* V1; %Drug in the lungs
+dydt(7) = k_cl*y(2)*V1 + k_met*y(2)*V1 + k_met*y(4)*V2; %Mass balance
 
 end
---
+++ runPatient.m
@@ -0,0 +1,65 @@
+function[T1,Y1,AUC,C_max,C_min] = runPatient(D0,dosageType)
+       
+    if(dosageType == "Gut")
+        D_g = D0 * .3;
+        D_l = 0;
+    elseif(dosageType == "Lung")
+        D_g = D0 * .5 * .3;
+        D_l = D0 * .5;
+    else
+        error("Invalid variable name: dosageType");
+    end
+    %% Parameters that might change
+    V1 = 2.5; %Need to pass weight in here for this variable to change.
+    
+
+    %% Parameters that probably won't
+    ka = log(2) / 2.77;
+    V2 = 1.5;
+    ka_lung = log(2)/1.5;
+    k12 = log(2) / 2.77;
+    k21 = k12;
+    k_abs = .693;
+    k_prod = .5; %NOTE THESE HAVE NO BASIS NEED TO CHANGE IT
+    k_met = .1; %NOTE THEIS HAS NO BASIS NEED TO CHANGE IT
+    k_cl = 0.2462;
+    k_cl_dop = 20.49;
+
+    %% Actual Simulation
+
+    y0 = [D_g 0 0 0 k_prod/k_met D_l 0]; %NOTE THERE NEEDS TO BE AN INITIAL DOPAMINE CONCENTRATION I NEED TO DO THE MATH ON THAT
+    p = [ka k_cl k_cl_dop k_met k_prod k_abs k12 k21 V1 V2 ka_lung];
+
+
+    options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
+    [T1,Y1] = ode45(@ldopa_eqns,[0 50],y0,options,p); % simulate model
+    MassBal = ((Y1 * [1 V1 0 V2 0 1 1].') - (D_g+D_l))/(D_g+D_l);
+    AUC = trapz(T1,Y1(:,5));
+    C_max = max(Y1(:,5));
+    C_min = min(Y1(:,5));
+
+    figure;
+    subplot(2,2,1);
+    plot(T1,Y1(:,2));
+    title("L-dopa Central")
+    xlabel("Hours")
+    ylabel("nM");
+
+    subplot(2,2,2);
+    plot(T1,Y1(:,4));
+    title("L-dopa Brain")
+    xlabel("Hours")
+    ylabel("nM");
+
+    subplot(2,2,3);
+    plot(T1,Y1(:,5));
+    title("Dopamine Brain")
+    xlabel("Hours")
+    ylabel("nM");
+
+    subplot(2,2,4);
+    plot(T1,MassBal);
+    title("Mass Balance");
+    xlabel("Hours")
+    ylabel("Deviation");
+end
